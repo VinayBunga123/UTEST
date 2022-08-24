47b5b3326bf8a7fb18b815d91c9fdefc7bcd4bed
Vinay <Vinay_Bunga@epam.com>
2022-08-24 12:36:44 +0530

    first commit

---
+++ Cal/MSTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MSTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MSTest")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("80961ccf-db81-4e39-ade6-d7e4366be0aa")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Cal/MSTest/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Cal;
+
+
+namespace MSTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Add()
+        {
+            Calculation c = new Calculation();
+            Assert.AreEqual(10, c.sum(5, 5));
+        }
+        [TestMethod]
+        /*[Ignore("Skip")]*/
+        public void sub()
+        {
+            Calculation c = new Calculation();
+            Assert.AreEqual(10, c.sub(15, 5));
+        }
+
+    }
+
+
+
+}
